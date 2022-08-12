d998fe1862194c17897d50fc565d497189a42780
JH_Kim <jimjack@naver.com>
2022-08-12 16:11:15 +0900

    TP-73 QNA 목록, 상세보기, 쓰기 페이지 작성
    [ 목록 ]
    기본 기능만 구현
    axios 사용해 qnaController 로부터 qnaBoard의 배열 취득
    이후 데이터 테이블에 표현
    페이징 및 검색 미구현
    
    [ 상세보기, 작성 ]
    일단 틀만 작성

---
+++ src/components/member/service/QnABoard.vue
@@ -0,0 +1,199 @@
+<!--
+  220812 김주현
+  문의글 게시판
+  
+  [ 문제점 ]
+    검색 기능 미구현
+-->
+
+<template>
+  <div class="main">
+    <div class="board">
+      <div class="board-header">
+        <h1>{{title}}</h1>
+      </div>
+      <div class="board-main">
+        <div class="board-search">
+          <!-- TODO 검색 기능(axios...) -->
+        </div>
+        <DataTable class="board-table" :value="list" responsiveLayout="scroll" :rows="10">
+            <Column field="brdNum" header="글번호" style="width: 100px;"></Column>
+            <Column field="title" header="제목" alignHeader="center">
+              <template #body="slotProps">
+                <a :href="getDetailLink(slotProps.data.brdNum)" v-text="slotProps.data.title" />
+              </template>
+            </Column>
+            <Column field="writerId" header="작성자" style="width: 100px;"></Column>
+            <Column field="wdate" header="작성일" style="width: 100px;"></Column>
+            <Column field="hit" header="조회수" style="width: 100px;"></Column>
+        </DataTable>
+      </div>
+      <div class="board-footer">
+        <Button color="#67AB9F">전체 글 보기(?)</Button>
+        <Button color="#67AB9F">공지 등록(??)</Button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import axios from 'axios'
+  import DataTable from 'primevue/datatable';
+  import Column from 'primevue/column';
+
+  export default
+  {
+    name: 'QnABoard',
+    components: {
+      DataTable, Column
+    },
+    props: {
+      title: String, // 타이틀
+      listLink: String, // 목록 링크
+      detailLink: String, // 자세히 보기 링크
+    },
+    data() {
+      return {
+        searchOpt: ["제목", "내용", "제목/내용"],
+        list: [],
+        curPage: 1,
+        pageCur: 1, // 현재 페이지 인덱스
+        pageEnd: 10 // 최대 페이지 수
+      }
+    },
+    created() {
+        this.list = this.getList();
+    },
+    mounted() {
+    },
+    methods: {
+      // 페이징: 최소값
+      getPageMin() {
+        return Math.max((this.pageCur - this.pageCur % 10), 0);
+      },
+      // 페이징: 최대값
+      getPageMax() {
+        return Math.min((this.pageCur - this.pageCur % 10) + 10, this.pageEnd);
+      },
+      // 페이징: 이전 페이지로 갈 수 있는지 여부
+      hasPrev() {
+        return (this.pageCur - this.pageCur % 10) > 0;
+      },
+      // 페이징: 다음 페이지로 갈 수 있는지 여부
+      hasNext() {
+        return (this.pageCur - this.pageCur % 10) > 0;
+      },
+      onSearch() {
+        alert("미구현");
+      },
+      onPageChange(n) {
+        this.curPage = n;
+        console.log(`${this.detailLink}?page=${this.curPage}`);
+      },
+      getList() {
+        this.list = axios.get(this.listLink, {
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          },
+          params: { }
+        }).then(function(resp) {
+          console.log(resp);
+          this.list = resp.data;
+          for (let el of this.list)
+          {
+            el.wdate = this.getDate(el.wdate);
+          }
+        }.bind(this));
+      },
+      getDetailLink(brdNum) {
+        return `${this.detailLink}?num=${brdNum}`;
+      },
+      getDate(ms) {
+        let date = new Date(ms);
+        return `${date.getUTCMonth() + 1}월 ${date.getUTCDate() + 1}일`;
+      }
+    }
+  }
+</script>
+<style scoped>
+  * {
+    padding: 20px;
+    margin: 0px; 
+    padding: 0px;
+  }
+  a {
+    text-decoration: none;
+  }
+  .main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+  }
+  .board {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 1080px;
+    border: 1px solid lightgray;
+  }
+  .board-header {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    height: 100px;
+    align-items: flex-start;
+    padding: 20px;
+  }
+  .board-header h1 {
+    font-size: 50px;
+    font-weight: bold;
+    color: #222;
+  }
+  .board-main {
+    display: flex;
+    flex-direction: column;
+    justify-content: top;
+    width: 100%;
+    align-items: center;
+    padding: 20px;
+  }
+  .board-search {
+    display: flex;
+    width: 100%;
+    justify-content: end;
+    align-items: right;
+  }
+  .board-search * {
+    margin: 0px 0px 0px 0px;
+  }
+  .board-search input[type="submit"] {
+    width: 100px;
+  }
+  .board-table {
+    width: 100%;
+    margin: 0px 0px 20px 0px;
+    text-align: center;
+    border-top: 1px solid gray;
+    border-collapse: collapse;
+  }
+  .board-page .currentPage {
+    color: #67AB9F;
+  }
+  .board-page * {
+    margin: 0px 4px;
+  }
+  .board-footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    width: 100%;
+    align-items: center;
+    padding: 20px;
+  }
+  .board-footer * {
+    margin: 0px 4px;
+  }
+</style>
---
+++ src/components/member/service/QnADetail.vue
@@ -0,0 +1,174 @@
+<!--
+  220810 김주현
+  가장 기본적인 게시판 상세보기 폼입니다.
+  
+  [ 프로퍼티 ]
+  detailLnk : String
+    게시글 조회를 시도할 때 GET 요청을 보낼 URL을 설정합니다.
+  title : String
+    폼 좌상단의 대문짝만한 문구
+  article : Object { number, title, id, date, content }
+    게시글을 표현합니다.
+    게시글 수정/삭제를 시도할 때, [ 해당링크 + "?num=" + number ] 의 형태로 GET/POST 요청을 보냅니다.
+  updateLink, deleteLink, listLink : String
+    각각 수정/삭제/목록으로 향하는 URL을 설정합니다.
+-->
+
+<template>
+  <div class="main">
+    <div class="board">
+      <div class="board-header">
+        <h1>{{title}}</h1>
+      </div>
+      <div class="board-main">
+        <table class="article-header" >
+          <tr>
+            <th style="text-align: start; padding-left: 20px;">{{article.title}}</th>
+            <th width="200px">{{article.id}}</th>
+            <th width="200px">{{article.date}}</th>
+          </tr>
+        </table>
+        <div class="article-main">
+          <div v-html="article.content"></div>
+        </div>
+        <div class="article-footer" />
+      </div>
+      <div class="board-footer">
+          <!-- TODO 버튼 구현 -->
+        <v-btn color="#67AB9F" @click="onUpdate">수정</v-btn>
+        <v-btn color="#67AB9F" @click="onDelete">삭제</v-btn>
+        <v-btn color="#67AB9F" @click="onList">목록으로</v-btn>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default
+  {
+    name: 'CommonBoard',
+    props: {
+      title: String, // 타이틀
+      article: { // 게시글 number, title, id, date, content
+        number: Number,
+        title: String,
+        id: String,
+        date: String,
+        content: String
+      },
+      updateLink: String, // 수정 링크
+      deleteLink: String, // 삭제 링크
+      listLink: String, // 목록 링크
+    },
+    computed: {
+      icon() {
+        return this.icons[this.iconIndex];
+      }
+    },
+    methods: {
+      onUpdate() {
+        alert("미구현");
+      },
+      onDelete() {
+        alert("미구현");
+      },
+      onList() {
+        alert("미구현");
+      },
+    }
+  }
+</script>
+<style scoped>
+  * {
+    padding: 20px;
+    font-family: 'Helvetica';
+    margin: 0px; 
+    padding: 0px;
+  }
+  a {
+    color: #333;
+    font-weight: bold;
+    text-decoration: none;
+  }
+  button, input[type="submit"] {
+    border: none;
+    font-weight: bold;
+    color: white;
+    background-color: #67AB9F;
+    padding: 5px;
+  }
+  .main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+  }
+  .board {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 1080px;
+    border: 1px solid lightgray;
+  }
+  .board-header {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    height: 100px;
+    align-items: flex-start;
+    padding: 20px;
+  }
+  .board-header h1 {
+    font-size: 50px;
+    font-weight: bold;
+    color: #222;
+  }
+  .board-main {
+    display: flex;
+    flex-direction: column;
+    justify-content: top;
+    width: 100%;
+    align-items: center;
+    padding: 20px;
+  }
+  .article-header {
+    width: 100%;
+    margin: 0px 0px 0px 0px;
+    text-align: center;
+    border-top: 1px solid gray;
+    border-collapse: collapse;
+  }
+  .article-header tr {
+    height: 55px;
+  }
+  .article-header th {
+    border-bottom: 1px solid gray;
+    font-size: 22px; /* 30px */
+    color: #222;
+  }
+  .article-main {
+    display: flex;
+    width: 100%;
+    margin: 20px 0px;
+    padding: 0px 20px;
+  }
+  .article-footer {
+    width: 100%;
+    margin: 0px 0px 20px 0px;
+    border-top: 1px solid gray;
+  }
+  .board-footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    width: 100%;
+    align-items: center;
+    padding: 20px;
+    margin: 0px 20px;
+  }
+  .board-footer * {
+    margin: 0px 4px;
+  }
+</style>
