26bb7625d986f87e0afa18b28ea7d71ffca95ee4
JH_Kim <jimjack@naver.com>
2022-08-12 16:11:15 +0900

    TP-73 QNA 목록, 상세보기, 쓰기 페이지 작성
    [ 목록 ]
    기본 기능만 구현
    axios 사용해 qnaController 로부터 qnaBoard의 배열 취득
    이후 데이터 테이블에 표현
    페이징이나 검색같은건 이제 만들어야함... 스읍
    
    [ 상세보기, 작성 ]
    일단 틀만 작성

---
+++ src/views/member/service/QnADetailView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <QnADetail 
+      :title="title"
+      :article="article"
+      :updateLink="updateLink"
+      :deleteLink="deleteLink"
+      :listLink="listLink"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import QnADetail from '@/components/member/service/QnADetail.vue'
+
+export default {
+  name: 'QnADetailView',
+  components: {
+    QnADetail
+  },
+  data() {
+    return {
+      title: "이곳에 타이틀 입력",
+      article: {
+        number: 1,
+        title: "aaaabbbbcccc",
+        id: "여기에 아이디 입력",
+        date: "1970.01.01",
+        content: "<p>testtest</p><p>testtest2</p><p>testtest3</p>"
+      },
+      updateLink: "/service/qna/write",
+      deleteLink: "/service/qna/delete",
+      listLink: "/service/qna/",
+    };
+  }
+}
+</script>
---
+++ src/views/member/service/QnAWriteView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <QnAWrite :title="title" :submitLink="submitLink" :cancelLink="cancelLink"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import QnAWrite from '@/components/member/service/QnAWrite.vue'
+
+export default {
+  name: 'QnAWriteView',
+  components: {
+    QnAWrite
+  },
+  data() {
+    return {
+      title: "이곳에 타이틀 입력",
+      submitLink: "/service/qna/write",
+      cancelLink: "/service/qna",
+    };
+  }
+}
+</script>
