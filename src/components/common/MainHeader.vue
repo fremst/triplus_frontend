29adca8e6552f8e80a7742c1b686787693f390f2
Jiyeon Kim <cleo0718@gmail.com>
2022-08-12 15:13:01 +0900

    TR-27 메인 페이지 헤더
    레이아웃만 구성, css X

---
+++ src/components/common/MainHeader.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="header">
+        <a href="/about">
+            <div class="loginLink">
+            로그인/회원가입
+            <Sidebar v-model:visible="visibleRight">
+	        Content
+            </Sidebar>
+            <Button icon="pi pi-arrow-left" @click="visibleRight = true" />
+            </div>
+        </a>
+        <a href="/">
+            <img :src="require('@/assets/logo.png')" class="logo"/>
+        </a>
+    </div>
+</template>
+
+<script>
+import Sidebar from 'primevue/sidebar';
+export default {
+  name: 'MainHeader',
+  components :{
+    Sidebar
+  },
+  data() {
+    return {
+        visibleRight: false
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+.logo {
+  height: 180px;
+  width: 200px;
+}
+.header {
+  height: 210px;
+  background: #91D9CC;
+}
+a {
+    text-decoration: none;
+}
+.loginLink {
+  text-align: right;
+  color: #333333;
+}
+</style>
