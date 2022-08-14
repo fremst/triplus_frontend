ad21602d09c6f6020bbf8f5ca1bc9b3f722525c2
Jiyeon Kim <cleo0718@gmail.com>
2022-08-15 04:44:00 +0900

    TR-111 API 숙박 카테고리 수정
    코드로 나오는 카테고리 수정
    JSON파일 참조

--- src/views/section/place/AccommodationView.vue
+++ src/views/section/place/AccommodationView.vue
@@ -1,82 +1,78 @@
 <template>
   <div class="wrap">
-    <h1>숙소조회</h1>
-    <div class="card">
-      <DataView :value="products" :layout="layout" :paginator="true" :rows="9" :sortOrder="sortOrder" :sortField="sortField">
-        <template #header>
-          <div class="grid grid-nogutter">
-            <div class="col-6" style="text-align: left">
-              <Dropdown v-model="sortKey" :options="sortOptions" optionLabel="label" placeholder="지역명 정렬" @change="onSortChange($event)"/>
+    <div class="place-inner">
+      <h1 class="section-title">숙소조회</h1>
+      <div class="card">
+        <DataView v-model:filters="filters1" :accomFilterFields="['title','addr1','tel','cat3']"
+                  :layout="layout"
+                  :paginator="true" :rows="9"
+                  :sortField="sortField" :sortOrder="sortOrder" :value="products">
+          <template #header>
+            <div class="flex justify-content-between">
+                <span class="p-input-icon-left">
+                    <i class="pi pi-search"/>
+                      <InputText v-model="filters1['search'].value" placeholder="검색어를 입력해 주세요."/>
+                  </span>
+              <Dropdown v-model="sortKey" :options="sortOptions" optionLabel="label" placeholder="숙소명 정렬"
+                        @change="onSortChange($event)"/>
             </div>
-            <div class="col-6" style="text-align: right">
-              <DataViewLayoutOptions v-model="layout" />
-            </div>
-          </div>
-        </template>
-        <template #list="slotProps">
-          <div class="col-12">
-            <div class="product-list-item">
-              <img :src='slotProps.data.firstimage' :alt="slotProps.data.title"/>
-              <div class="product-list-detail">
-                <div class="product-name">{{slotProps.data.title}}</div>
-                <div class="product-description">{{slotProps.data.addr1}}{{slotProps.data.addr2}}</div>
-                <div class="product-description">{{slotProps.data.tel}}</div>
-                <i class="pi pi-tag product-category-icon"></i><span class="product-category">{{slotProps.data.cat1}}</span>
-              </div>
-              <div class="product-list-action">
-              </div>
-            </div>
-          </div>
-        </template>
+          </template>
+          <template #empty>
+            <p>검색결과가 없습니다.</p>
+          </template>
 
-        <template #grid="slotProps">
-          <div class="col-12 md:col-4">
-            <div class="product-grid-item card">
-              <div class="product-grid-item-top">
-                <div>
-                  <i class="pi pi-tag product-category-icon"></i>
-                  <span class="product-category">{{slotProps.data.cat1}}</span>
+          <template #list="slotProps">
+            <div class="col-12">
+              <div class="product-list-item">
+                <img :alt="slotProps.data.title" :src='slotProps.data.firstimage'/>
+                <div class="product-list-detail">
+                  <div class="product-name">{{ slotProps.data.title }}</div>
+                  <div class="product-description">{{ slotProps.data.addr1 }}{{ slotProps.data.addr2 }}</div>
+                  <div class="product-description">{{ slotProps.data.tel }}</div>
+                  <i class="pi pi-tag product-category-icon"></i><span
+                    class="product-category">{{ parseCategory(slotProps.data.cat3) }}</span>
+                </div>
+                <div class="product-list-action">
                 </div>
               </div>
-              <div class="product-grid-item-content">
-                <img :src='slotProps.data.firstimage' :alt="slotProps.data.title"/>
-                <div class="product-name">{{slotProps.data.title}}</div>
-                <div class="product-description">,{{slotProps.data.addr1}},{{slotProps.data.addr2}}</div>
-              </div>
             </div>
-          </div>
-        </template>
-      </DataView>
+          </template>
+        </DataView>
+      </div>
     </div>
   </div>
 </template>
 <script>
 import axios from 'axios';
 import Dropdown from "primevue/dropdown";
-import DataViewLayoutOptions from "primevue/dataviewlayoutoptions"
+import {FilterMatchMode, FilterOperator} from 'primevue/api';
+import placeCategory from '@/views/section/place/placeCategory.json';
 
 export default {
   data() {
     return {
       products: null,
-      layout: 'grid',
+      layout: 'list',
       sortKey: null,
       sortOrder: null,
       sortField: null,
+      filters1: null,
       sortOptions: [
-        {label: '이름 내림차순', value: '!price'},
-        {label: '이름 올림차순', value: 'price'},
+        //숙소명으로 정렬하기
+        {label: '이름 올림차순', value: 'title'},
+        {label: '이름 내림차순', value: '!title'},
       ],
     }
   },
   created() {
+    this.initFilters1();
   },
   mounted() {
     this.getList();
   },
   methods: {
     getList() {
-      axios.get('https://apis.data.go.kr/B551011/KorService/searchStay?serviceKey=q78jx2pDdopDli5F6rUCzaNdpTo1BPB5p%2FhBDuJQVUkaf89mwtC5fauNUQs2BFE50udejJt%2FGaKOkZ3Y9F28OA%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=Triplus&_type=json&listYN=Y&arrange=A', this.data, {
+      axios.get('https://apis.data.go.kr/B551011/KorService/searchStay?serviceKey=q78jx2pDdopDli5F6rUCzaNdpTo1BPB5p%2FhBDuJQVUkaf89mwtC5fauNUQs2BFE50udejJt%2FGaKOkZ3Y9F28OA%3D%3D&numOfRows=30&pageNo=1&MobileOS=ETC&MobileApp=Triplus&_type=json&listYN=Y&arrange=Q&hanOk=0&benikia=0&goodStay=0', this.data, {
         headers: {
           "Access-Control-Allow-Origin": "*",
         }
@@ -86,7 +82,7 @@ export default {
         console.log(err.response);
       });
     },
-    onSortChange(event){
+    onSortChange(event) {
       const value = event.value.value;
       const sortValue = event.value;
 
@@ -94,18 +90,37 @@ export default {
         this.sortOrder = -1;
         this.sortField = value.substring(1, value.length);
         this.sortKey = sortValue;
-      }
-      else {
+      } else {
         this.sortOrder = 1;
         this.sortField = value;
         this.sortKey = sortValue;
       }
     },
-  },
-      components:{
-      Dropdown,
-      DataViewLayoutOptions
+    //관광API 카테고리 변경
+    parseCategory(inputCat3) {
+      let result = placeCategory.categoryData.filter(function (object) {
+        return object['cat3'] === inputCat3;
+      })[0];
+      if (result) {
+        return result.category
+      } else {
+        //설정해 두지 않은 카테고리가 나올 시, 기타로 처리.
+        return '기타';
+      }
+    },
+    initFilters1() {
+      this.filters1 = {
+        'search': {value: null, matchMode: FilterMatchMode.CONTAINS},
+        'title': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.STARTS_WITH}]},
+        'addr1': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.STARTS_WITH}]},
+        'tel': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.STARTS_WITH}]},
+        'cat3': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.STARTS_WITH}]},
+      }
     }
+  },
+  components: {
+    Dropdown
+  }
 }
 </script>
 
@@ -114,10 +129,16 @@ export default {
 .card {
   background: #ffffff;
   padding: 1rem;
-  box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);
+  box-shadow: 0 2px 1px -1px rgba(0, 0, 0, .2), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12);
   border-radius: 4px;
   margin-bottom: 2rem;
 }
+
+.p-inputtext {
+  width: 400px;
+  text-align: left;
+}
+
 .p-dropdown {
   width: 14rem;
   font-weight: normal;
@@ -137,6 +158,7 @@ export default {
   vertical-align: middle;
   margin-right: .5rem;
 }
+
 .product-category-icon {
   vertical-align: middle;
   margin-right: .5rem;
@@ -147,6 +169,7 @@ export default {
   vertical-align: middle;
   font-size: 14pt;
 }
+
 .product-badge {
   border-radius: 2px;
   padding: .25em .5rem;
@@ -170,7 +193,8 @@ export default {
   background: #FEEDAF;
   color: #8A5340;
 }
-::v-deep(.product-list-item) {
+
+.product-list-item {
   display: flex;
   align-items: center;
   padding: 1rem;
@@ -178,6 +202,7 @@ export default {
 
   img {
     width: 200px;
+    height: 200px;
     box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
     margin-right: 2rem;
   }
@@ -207,32 +232,6 @@ export default {
   }
 }
 
-::v-deep(.product-grid-item) {
-  margin: .5rem;
-  border: 1px solid var(--surface-border);
-
-  .product-grid-item-top,
-  .product-grid-item-bottom {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  img {
-    width: 290px;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-    margin: 2rem 0;
-  }
-
-  .product-grid-item-content {
-    text-align: center;
-  }
-
-  .product-price {
-    font-size: 1.5rem;
-    font-weight: 600;
-  }
-}
 
 @media screen and (max-width: 576px) {
   .product-list-item {
@@ -265,5 +264,21 @@ export default {
     }
 
   }
+}
+
+.wrap {
+  width: 100%;
+}
+
+.place-inner {
+  width: 1080px;
+  margin: 0 auto;
+}
+
+.section-title {
+  margin-top: 20px;
+  margin-left: 20px;
+  font-weight: bold;
+  font-size: 30px;
 }
 </style>
