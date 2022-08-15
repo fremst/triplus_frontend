697119f779531f9dc8169da3876489fd9ed52e10
Chankyu Kim <chkiya@gmail.com>
2022-08-16 04:10:14 +0900

    TR-44 패키지 결제(예약) 페이지 작성
    - 예약 기본 폼 작성
    - 결제에 필요한 파라미터 송신하는 폼(PayView) 작성 (hidden 처리)
    - axios 일부 연동

---
+++ src/components/section/package/reservation/PackageReservationItemInfo.vue
@@ -0,0 +1,186 @@
+<template>
+  <h1>상품 정보</h1>
+  <table>
+    <tr>
+      <td>상품번호</td>
+      <td>100</td>
+    </tr>
+    <tr>
+      <td>상품명</td>
+      <td>xx</td>
+    </tr>
+    <tr>
+      <td>여행 기간</td>
+      <td>xx</td>
+    </tr>
+    <tr>
+      <td>출발일</td>
+      <td>xx</td>
+    </tr>
+    <tr>
+      <td>도착일</td>
+      <td>xx</td>
+    </tr>
+    <tr>
+      <td>여행자</td>
+      <td>성인 xx명 / 아동 xx명</td>
+    </tr>
+  </table>
+  <br>
+  <h1>예약자 정보</h1>
+  <table>
+    <tr>
+      <td>이름</td>
+      <td><InputText type="text" class="p-inputtext-sm" v-model="value1"/></td>
+      <td>생년월일</td>
+      <td><InputText type="text" class="p-inputtext-sm" v-model="value1" /></td>
+      <td>성별</td>
+      <td>
+        <RadioButton id="male" name="gender" value="male" v-model="gender" />
+        <label for="male">남성</label>
+        <br>
+        <RadioButton id="female" name="gender" value="female" v-model="gender" />
+        <label for="female">여성</label>
+      </td>
+    </tr>
+    <tr>
+      <td>이메일</td>
+      <td colspan="5">
+        <InputText type="text" class="p-inputtext-sm" v-model="value1" />
+        &nbsp;@&nbsp;
+        <Dropdown v-model="selectedDomain" class="p-inputtext-sm" :options="domains" editable optionLabel="name" placeholder="직접입력" />
+      </td>
+    </tr>
+    <tr>
+      <td>휴대폰 번호</td>
+      <td colspan="4">
+        <InputText type="text" class="p-inputtext-sm" v-model="value1" />
+      </td>
+    </tr>
+  </table>
+
+  <h1>일행 정보</h1>
+
+  <table v-for="(person, index) in list" :key="index">
+    <tr>
+      <td>
+        구분
+      </td>
+      <td>
+        {{person.trvlrClass}}
+      </td>
+    </tr>
+    <tr>
+      <td>이름</td>
+      <td><InputText type="text" class="p-inputtext-sm" :value="person.name" v-model="value1" /></td>
+    </tr>
+    <tr>
+      <td>휴대폰 번호</td>
+      <td>
+        <InputText type="text" class="p-inputtext-sm" v-model="value1" />
+      </td>
+    </tr>
+  </table>
+
+  <h1>할인 쿠폰 선택</h1>
+  <table>
+    <tr>
+      <td>적용된 쿠폰</td>
+      <td>
+        <Dropdown v-model="selectedCoupon" class="p-inputtext-sm" :options="coupons" optionLabel="name" placeholder="쿠폰 선택"
+        empty-message="사용 가능한 쿠폰 없음"/>
+        &nbsp;
+        <Button class="p-button-sm">
+          쿠폰 적용
+        </Button>
+      </td>
+    </tr>
+<!--    <tr>-->
+<!--      <td>할인금액</td>-->
+<!--      <td>0 원</td>-->
+<!--    </tr>-->
+  </table>
+  <br>
+
+  <div class="box">
+    <table>
+      <tr>
+        <td class="td-price">
+          상품 금액
+        </td>
+        <td class="td-price">
+          할인 금액
+        </td>
+        <td class="td-price">
+          총액
+        </td>
+      </tr>
+      <tr>
+        <td class="td-price">
+          xx 원
+        </td>
+        <td class="td-price">
+          xx 원
+        </td>
+        <td class="td-price">
+          xx 원
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sales: null,
+      gender: 'male',
+      list: [{name: '가성인', trvlrClass: '성인'}, {name: '나성인', trvlrClass: '성인'}, {name: '가아동', trvlrClass: '아동'}],
+      selectedDomain: "",
+      selectedCoupon: "없음",
+      domains: [
+        {name: 'naver.com', value: 'naver.com'},
+        {name: 'gmail.com', value: 'gmail.com'},
+        {name: 'daum.net', value: 'daum.net'},
+        {name: 'nate.com', value: 'nate.com'}
+      ],
+      coupons: [
+        // {name: '없음', value: '0'}
+      ]
+    }
+  },
+  methods: {
+    formatCurrency(value) {
+      return value.toLocaleString('ko-KR')+' 원';
+    }
+  },
+}
+</script>
+
+<style scoped>
+.box{
+  border: 1px solid #dee2e6;
+  padding: 30px 20px;
+}
+table{
+  width: 100%;
+  border-collapse: collapse;
+}
+tr{
+  border-top: 1px solid #dee2e6;
+  border-bottom: 1px solid #dee2e6;
+  height: 50px;
+}
+td{
+  padding: 15px;
+  text-align: left;
+}
+td:not(:last-child){
+  border-right: 1px solid #dee2e6;
+}
+.td-price{
+  text-align: center;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -7,8 +7,11 @@ import PrimeVue from "primevue/config";
 import InputText from "primevue/inputtext";
 import Button from "primevue/button";
 
+import RadioButton from 'primevue/radiobutton';
+import Dropdown from 'primevue/dropdown';
+
 //테마설정
-import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/themes/lara-light-teal/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 
@@ -16,11 +19,13 @@ const app = createApp(App);
 
 app.use(store);
 app.use(router);
-
 app.use(PrimeVue);
 
 // PrimeVue의 컴포넌트를 전역으로 사용하고 싶을땐 여기에 등록해서 사용한다.
 app.component("InputText", InputText);
 app.component("Button", Button);
+
+app.component("RadioButton", RadioButton);
+app.component("Dropdown", Dropdown);
 
 app.mount("#app");
