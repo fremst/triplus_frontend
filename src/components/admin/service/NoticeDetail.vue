8c2a8625866c93089c199edb420f646d191813d3
Mingi1029 <alsrl1406@naver.com>
2022-08-16 09:50:58 +0900

    TR-84 공지사항 목록,등록,수정,상세 페이지 레이아웃
    공지사항
     - 목록 리스트 레이아웃
     페이징 처리 미구현, 검색 기능 미구현
     axios 미구현
     - 등록, 수정 레이아웃 (Editor)
     등록 기능 미구현
     - 상세 레이아웃
     삭제 기능 미구현
    
    public index.html에 부트스트랩 설정
    router index.js에 경로 지정

---
+++ src/components/admin/service/NoticeDetail.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="container">
+        <table class="table">
+            <thead>
+            <tr>
+                <th colspan="3" style="width: 80%" id="title"><span>[{{category}}] </span>{{title}}</th>
+                <th colspan="3" style="width: 10%">{{regdate}}</th>
+                <th colspan="3" style="width: 10%">{{hit}}</th>
+
+
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td colspan="10" style="height: 600px; text-align: left;">
+                    <pre>{{contents}}</pre>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <div id="btnGroup">
+            <Button color="#67AB9F" id="updateBtn" @click="pageUpdate" label="수정" />
+            <Button color="#67AB9F" id="deleteBtn" @click="delNotice" label="삭제" />
+            <Button color="#67AB9F" id="listBtn" @click="pageList" label="목록으로" />
+        </div>
+    </div>
+</template>
+
+<script>
+   // import axios from 'axios'
+    export default {
+        name: "NoticeDetail.vue",
+        data(){
+            return{
+                category:"기타",
+                brdnum:0,
+                title:"입력된 제목이 나타남",
+                regdate:"2022/08/15",
+                hit: 10,
+                contents:"안녕하세요, 네이버웍스입니다.\n" +
+                    "\n" +
+                    " \n" +
+                    "\n" +
+                    "네이버웍스 드라이브 서비스의 서버 작업이 8/4일 예정되어 있습니다.\n" +
+                    "고객께서는 서버 작업 중에 드라이브 서비스 접속 시 순단 현상을 경험할 수 있으니,\n" +
+                    "자세한 내용 아래 확인 부탁드립니다."
+
+            }
+        },
+        methods:{
+            pageUpdate(){
+                this.$router.push({ path: 'update' });
+            },
+            pageList(){
+                this.$router.push({ path: 'list' });
+            },
+            delNotice(){
+                alert("삭제 미구현");
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+    #title{
+        text-align: left;
+    }
+    #updateBtn{
+        margin-right: 10px;
+    }
+    #deleteBtn{
+        margin-right: 10px;
+    }
+    #btnGroup{
+        padding-left: 800px;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,28 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/HomeView.vue")
+  },
+  {
+    path:"/service/notice/list",
+    name:"notice",
+    component: () => import("@/views/admin/service/NoticeListView.vue")
+  },
+  {
+    path:"/service/notice/write",
+    name:"notice-write",
+    component: () => import("@/views/admin/service/NoticeWriteView.vue")
+  },
+  {
+    path:"/service/notice/detail",
+    name:"notice-detail",
+    component: () => import("@/views/admin/service/NoticeDetailView.vue")
+  },
+  {
+    path:"/service/notice/update",
+    name:"notice-update",
+    component: () => import("@/views/admin/service/NoticeUpdateView.vue")
   }
+
 ];
 
 const router = createRouter({
