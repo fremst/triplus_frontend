35fcaf043c967146eeb3fd9a3ec550ff0127fc94
Mingi1029 <alsrl1406@naver.com>
2022-08-23 15:40:24 +0900

    TR-160-F 메거진 목록, 상세보기
    메거진 목록 레이아웃
    - 검색, 페이징 프론트 처리 완료
    메거진 상세내용 레이아웃

---
+++ src/views/section/magazine/MagazineDetailView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <MagazineDetail></MagazineDetail>
+    </div>
+</template>
+
+<script>
+    import MagazineDetail from "@/components/section/magazine/MagazineDetail";
+    export default {
+        name: "MagazineDetailView.vue",
+        components: {
+            MagazineDetail
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
