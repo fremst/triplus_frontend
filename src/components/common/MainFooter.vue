92ca8084183997e9dcbb00398e82738621822c75
zhtmr1 <zhtmr1@gmail.com>
2022-08-24 13:35:57 +0900

    TR-30-f-메인페이지-수정-css
    - Header css 수정
    - Footer css 수정
    - 메인 슬라이드 margin값 변경
    - logo 이미지 추가 및 변경

--- src/components/common/MainFooter.vue
+++ src/components/common/MainFooter.vue
@@ -1,7 +1,22 @@
 <template>
-    <div class="footer">
-        <img :src="require('@/assets/logo.png')" class="logo"/>
-    </div>
+  <div class="wrap">
+      <div class="footer">
+        <div class="foote-logo">
+          <img :src="require('@/assets/logo_b.png')" class="logo"/>
+        </div>
+        <div class="footer-msg">
+          <h3> 트리플러스 </h3>
+          <br>
+          <h4> 이용 약관 및 면책 조항    트리플러스 이용약관    개인정보 처리방침    취소 및 환불 정책</h4>
+          <p>주소 : 우편번호 (00000) 서울특별시 강남구 삼성로 512 삼성동빌딩 10층<br>
+              (주) 트리플러스 이메일 : triplus@travle.co.kr 
+              개인정보관리책임자 : 김트리플러스 TEL : 02-000-0000 FAX : 02-000-0000 <br>
+              고객만족실 : 1588-0000 멤버십 문의 : 1588-1111 <br>
+              Copyright (c) 2022 TRIPLUS. All Rights Reserved.
+          </p>
+        </div>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -12,12 +27,30 @@ export default {
 
 <style scoped>
 .logo {
-  height: 100px;
-  width: 120px;
+  height: 150px;
+  width: 170px;
+  margin-top: 30px;
 }
 .footer {
-  height: 180px;
-  background: #91D9CC;
+  height: 200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  border-top: 1px solid #bdbdbd ;
+}
+.footer-msg{
+  margin: 30px;
+}
+h4{
+  color: #bdbdbd;
+}
+h3{
+  color: #bdbdbd;
+}
+p{
+  font-size: 12px;
+  color: #bdbdbd;
 }
 a {
   text-decoration: none;
--- src/components/home/MainSlider.vue
+++ src/components/home/MainSlider.vue
@@ -67,5 +67,6 @@ export default ({
     margin: 0 auto;
     width: 1080px;
     position: relative;
+    margin-top: 5%;
   }
 </style>
