13843d7a562584ecbd0cc40dc6f853c625b55207
Jiyeon Kim <cleo0718@gmail.com>
2022-09-05 07:26:36 +0900

    TR-133-f 일정등록
    오늘 날씨 조회 api등록, 프론트 구현(날씨, 기온, 도시명 조회) 추후 수정 필
    axios header 수정, env파일 api key 추가

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "/wid": {
+        target: "http://www.kma.go.kr"
+        //changeOrigin: true
+      }
+    }
+  }
 });
